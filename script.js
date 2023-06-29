cd1ce7b0fefc217a7410f02fb676de9bb6828363
Akash srinivasan <114242375+akashfullstackdeveloper@users.noreply.github.com>
2023-06-29 13:50:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let select = document.querySelectorAll('.currency')
+let btn = document.getElementById('btn')
+let input = document.getElementById('input')
+let error=document.getElementById("error")
+let container=
+fetch('https://api.frankfurter.app/currencies')
+.then(res=>res.json())
+.then(res=>displayDropDown(res))
+
+function displayDropDown(res){
+  //console.log(Object.entries(res)[2][0])
+  let curr = Object.entries(res)
+  for(let i=0;i<curr.length;i++){
+    let opt = `<option value="${curr[i][0]}">${curr[i][0]}</option>`
+    select[0].innerHTML += opt
+    select[1].innerHTML += opt
+  }
+}
+
+btn.addEventListener('click',()=>{
+  let curr1 = select[0].value
+  let curr2 = select[1].value
+  let inputVal = input.value
+  if(curr1===curr2){
+    document.querySelector(".container").style.border = "5px solid red";
+    error.innerHTML="plese enter the diffrent curency";
+  }
+
+    
+  else
+    convert(curr1,curr2,inputVal)
+});
+
+function convert(curr1,curr2,inputVal){
+    document.querySelector(".container").style.border = "5px solid green";
+    error.innerHTML="";
+  const host = 'api.frankfurter.app';
+  fetch(`https://${host}/latest?amount=${inputVal}&from=${curr1}&to=${curr2}`)
+  .then(resp => resp.json())
+  .then((data) => {
+    document.getElementById('result').value = Object.values(data.rates)[0]
+  });
+
+}
